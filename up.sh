89b28e3d56f2f73dfc198326077317ccdd2c4269
Gokhan Uslu <gu@nordsense.com>
2018-02-15 20:55:10 +0100

    updts

--- up.sh
+++ up.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 yarn &&
 node_modules/.bin/ng build --prod --aot &&
-docker build -t filmsfr-web . && docker run -p 8080:8080 filmsfr-web &&
-chrome http://localhost:8080
+docker build -t filmsfr-web . && docker run -p 8080:80 filmsfr-web &&
