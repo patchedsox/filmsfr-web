b28227ad1ff026d72691062f4300a96b6aff9291
Gokhan Uslu <gu@nordsense.com>
2018-02-16 10:40:13 +0100

    bfix for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'localhost:3333',
+  apiUrl: 'ec2-34-240-82-42.eu-west-1.compute.amazonaws.com:3333',
   http: 'http://',
   mapboxToken: 'pk.eyJ1IjoiZy1tYW4iLCJhIjoiY2pkZzhzNm9vMGJ5ZzJ3bnoyd2NsaW12MCJ9.Ujhx7KNL5irrxpCOFS3wAA'
 };
